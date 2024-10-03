0d60f12d53a17c1fa5f0e0ff07c0502ba2d93a9f
Daniel <122674836+DanielBucayan@users.noreply.github.com>
2024-10-03 06:33:54 +0000

    reverted web.php

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/', 'home')->name('home');
-Route::view('contact', 'contact')->name('contact');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
