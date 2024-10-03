a6f8d86d3c47108facd264a5787ff99c4821d430
Daniel <122674836+DanielBucayan@users.noreply.github.com>
2024-10-03 06:26:31 +0000

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
-
 $url = config('app.url');
 URL::forceRootUrl($url);
-
 Route::get('/', function () {
     return view('welcome');
 });
