08a9e3a008272fb1eb5bba6f1f3debe9b2b11409
Daniel <122674836+DanielBucayan@users.noreply.github.com>
2024-10-03 06:33:14 +0000

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('/', 'home')->name('home');
+Route::view('contact', 'contact')->name('contact');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
